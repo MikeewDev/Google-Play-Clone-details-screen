94e54f1f4ef6f68982abfe2052e358a0b7bb8bc3
MIKE <142926964+MikeewDev@users.noreply.github.com>
2023-11-06 00:16:07 -0300

    Update script.js
    Fixes

--- script.js
+++ script.js
@@ -1,25 +1,10 @@
-  document.getElementById('gerarLink').addEventListener('click', function() {
-    const ddd = document.getElementById('ddd').value;
-    const numero = document.getElementById('numero').value;
-    const mensagem = encodeURIComponent(document.getElementById('mensagem').value);
-
-    if (ddd && numero && mensagem) {
-      const link = `https://wa.me/${ddd}${numero}?text=${mensagem}`;
-      document.querySelector('.text-link').innerHTML = `<p><a href="#" id="generatedLink">${link}</a></p>`;
-    } else {
-      alert('Please fill in all the required fields.');
-      document.querySelector('.text-link').innerHTML = '<p>Link (Click to copy)</p>';
-    }
-  });
-
-  document.querySelector('.text-link').addEventListener('click', function(event) {
-    if (event.target.id === 'generatedLink') {
-      const tempInput = document.createElement('input');
-      tempInput.value = event.target.textContent;
-      document.body.appendChild(tempInput);
-      tempInput.select();
-      document.execCommand('copy');
-      document.body.removeChild(tempInput);
-      alert('Link copied to clipboard.');
-    }
-  });
+function addBoxShadowOnScroll() {
+  var navbar = document.getElementById("navbar");
+  var scrollPosition = window.scrollY;
+  if (scrollPosition > 10) { // Altere esse valor para determinar quando o box-shadow deve ser aplicado
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+}
+window.addEventListener("scroll", addBoxShadowOnScroll);
